16e20602391d4fea100d9a91e4c250ae01842649
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-12-19 01:51:08 +0200

    swiper styled

--- src/components/Swiper/Swiper.jsx
+++ src/components/Swiper/Swiper.jsx
@@ -1,23 +1,68 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay, Scrollbar, A11y } from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { useRef } from 'react';
+import Svg from '../Svg/Svg';
 
-export const SwiperComponent = ({images}) => {
+export const SwiperComponent = ({ images, id, name }) => {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
   return (
-<>
-{images &&    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {images.map(el => (
-        <SwiperSlide>
-          <img src={el} alt="slide" />
-        </SwiperSlide>
-      ))}
-    </Swiper>}</>
+    <>
+      {images && (
+        <Swiper
+          className="flex justify-center items-center lg:w-[800px]"
+          modules={[Navigation, Autoplay, Scrollbar, A11y]}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          spaceBetween={50}
+          slidesPerView={1}
+          speed={940}
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+        >
+          {images.map((el) => (
+            <SwiperSlide
+              key={id}
+              className="mb-5 w-full h-[450px] flex items-center justify-center"
+            >
+              <img className="w-full md:w-[450px]" src={el} alt={name} />
+            </SwiperSlide>
+          ))}
+          <button
+            className="flex justify-center items-center absolute z-10 top-[50%] w-[25px] h-[25px] bg-black rounded-[50%]  left-[10px] opacity-70"
+            ref={prevRef}
+          >
+            <div className=" rotate-180">
+              <Svg
+                id={'icon-arrow'}
+                size={24}
+                stroke={'white'}
+                fill={'white'}
+              />
+            </div>
+          </button>
+          <button
+            className="flex justify-center items-center absolute z-10 top-[50%] w-[25px] h-[25px] bg-black  opacity-70 rounded-[50%] right-[10px]"
+            ref={nextRef}
+            onClick={() => console.log(1)}
+          >
+            <Svg id={'icon-arrow'} size={24} stroke={'white'} fill={'white'} />
+          </button>
+        </Swiper>
+      )}
+    </>
   );
 };
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -4,7 +4,10 @@ import { StarIcon } from '@heroicons/react/20/solid';
 import { RadioGroup } from '@headlessui/react';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProductById } from '../../Redux/products/productsOperation';
+import {
+  fetchProducts,
+  getProductById,
+} from '../../Redux/products/productsOperation';
 import {
   getIsLoadingProducts,
   getSelectedProducts,
@@ -69,15 +72,23 @@ const Product = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const product = useSelector(getSelectedProducts);
+  const allProducts = useSelector(fetchProducts);
   const isLoading = useSelector(getIsLoadingProducts);
 
   useEffect(() => {
     dispatch(getProductById(id));
+    dispatch(fetchProducts());
   }, [dispatch]);
   console.log(product);
 
-  // const [selectedColor, setSelectedColor] = useState(product.colors[0]);
-  // const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+  // const anotherColors = allProducts.filter((el) => {
+  //   const isNotDuplicate = el._id !== product._id;
+  //   const sameType = isNotDuplicate.name === product.name;
+  //   return sameType;
+  // });
+
+  // console.log(anotherColors);
+  console.log(allProducts);
   const [count, setCount] = useState(0);
 
   return (
@@ -86,10 +97,13 @@ const Product = () => {
       {product && !isLoading ? (
         <div className="pt-6">
           {/* Image gallery */}
-   <div className="">
-   <SwiperComponent images={[product.mainPhoto, ...product.extraPhotos]}/>
-
-   </div>
+          <div className="">
+            <SwiperComponent
+              images={[product.mainPhoto, ...product.extraPhotos]}
+              id={product._id}
+              name={product.name}
+            />
+          </div>
           {/* Product info */}
           <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
             <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
@@ -135,46 +149,10 @@ const Product = () => {
 
               <form className="mt-10">
                 {/* Colors */}
-                {/* <div>
+                <div>
                   <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                  <RadioGroup
-                    value={selectedColor}
-                    onChange={setSelectedColor}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a color
-                    </RadioGroup.Label>
-                    <div className="flex items-center space-x-3">
-                    {product.colors.map((color) => (
-                      <RadioGroup.Option
-                        key={color.name}
-                        value={color}
-                        className={({ active, checked }) =>
-                          classNames(
-                            color.selectedClass,
-                            active && checked ? 'ring ring-offset-1' : '',
-                            !active && checked ? 'ring-2' : '',
-                            'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none'
-                          )
-                        }
-                      >
-                        <RadioGroup.Label as="span" className="sr-only">
-                          {color.name}
-                        </RadioGroup.Label>
-                        <span
-                          aria-hidden="true"
-                          className={classNames(
-                            color.class,
-                            'h-8 w-8 rounded-full border border-black border-opacity-10'
-                          )}
-                        />
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                  </RadioGroup>
-                </div> */}
+                  <div className="flex"></div>
+                </div>
 
                 {/* Количество */}
                 <div className="mt-10">
@@ -224,7 +202,7 @@ const Product = () => {
                   type="submit"
                   className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  Add to bag
+                  Добавить в корзину
                 </button>
               </form>
             </div>
@@ -232,7 +210,7 @@ const Product = () => {
             <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
               {/* Description and details */}
               <div>
-                <h3 className="sr-only">Description</h3>
+                <h3 className="">Description</h3>
 
                 <div className="space-y-6">
                   <p className="text-base text-gray-900">
@@ -240,7 +218,6 @@ const Product = () => {
                   </p>
                 </div>
               </div>
-
 
               <div className="mt-10">
                 <h2 className="text-sm font-medium text-gray-900">Details</h2>

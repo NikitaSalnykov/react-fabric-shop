8f3feab932bcf613f70c6a7582bac1eb33e50060
NikitaSalnykov <121830017+NikitaSalnykov@users.noreply.github.com>
2024-01-15 16:02:02 +0000

    Deploying to gh-pages from @ NikitaSalnykov/react-fabric-shop@d3aaca834eaf5c8151fa4f186712ab2a0b2b9641 🚀

--- assets/AdminUsers-47e74b3b.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-f7863717.js";const e=()=>s.jsx("div",{children:"AdminUsers"});export{e as default};
---
+++ assets/postsSelectors-a2925d28.js
@@ -0,0 +1,1 @@
+const o=s=>s.posts.posts,t=s=>s.posts.isLoading;export{t as a,o as g};

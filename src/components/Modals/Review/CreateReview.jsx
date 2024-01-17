190f9f7219a96d21c0cbbc1a7832be22af83398d
NikitaSalnykov <salnikov.nkt@gmail.com>
2024-01-17 18:27:50 +0200

    fix ReviewSchema

--- src/components/Modals/Review/CreateReview.jsx
+++ src/components/Modals/Review/CreateReview.jsx
@@ -143,32 +143,32 @@ export const CreateReview = ({onCloseModal}) => {
               </p>
           <div className="flex items-center space-x-1 rtl:space-x-reverse">
         <div className=" cursor-pointer">
-        <svg onClick={() => formik.setFieldValue('rating', 1)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.rating >= 1 ? "gold" : "#dadada"} viewBox="0 0 22 20">
+        <svg onClick={() => formik.setFieldValue('rating', 1)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.values.rating >= 1 ? "gold" : "#dadada"} viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
         </svg>
         </div>
         <div className="cursor-pointer">
-        <svg onClick={() => formik.setFieldValue('rating', 2)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.rating >= 2 ? "gold" : "#dadada"} viewBox="0 0 22 20">
+        <svg onClick={() => formik.setFieldValue('rating', 2)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.values.rating >= 2 ? "gold" : "#dadada"} viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
         </svg>
         </div>
         <div className="cursor-pointer">
-        <svg onClick={() => formik.setFieldValue('rating', 3)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.rating >= 3 ? "gold" : "#dadada"} viewBox="0 0 22 20">
+        <svg onClick={() => formik.setFieldValue('rating', 3)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.values.rating >= 3 ? "gold" : "#dadada"} viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
         </svg>
         </div>
         <div className="cursor-pointer">
-        <svg onClick={() => formik.setFieldValue('rating', 4)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.rating >= 4 ? "gold" : "#dadada"} viewBox="0 0 22 20">
+        <svg onClick={() => formik.setFieldValue('rating', 4)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.values.rating >= 4 ? "gold" : "#dadada"} viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
         </svg>
         </div>
         <div className="cursor-pointer">
-        <svg onClick={() => formik.setFieldValue('rating', 5)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.rating >= 5 ? "gold" : "#dadada"} viewBox="0 0 22 20">
+        <svg onClick={() => formik.setFieldValue('rating', 5)} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={formik.values.rating >= 5 ? "gold" : "#dadada"} viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
         </svg>
         </div>
         {errors['rating'] && (
-              <p className={`${errorTextStyle} right-0`}>{errors['rating']}</p>
+              <p className={`${errorTextStyle} left-0 bottom-[-18px]`}>{errors['rating']}</p>
             )}
     </div>
           </div>

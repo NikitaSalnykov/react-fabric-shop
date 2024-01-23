102d5c815d2a661e12347e7a9ab8928501f2cb8f
NikitaSalnykov <salnikov.nkt@gmail.com>
2024-01-23 19:00:53 +0200

    Update AdminOrders.jsx

--- src/pages/Admin/AdminOrders.jsx
+++ src/pages/Admin/AdminOrders.jsx
@@ -9,6 +9,7 @@ import { Menu, Transition } from '@headlessui/react';
 import { formattedDate } from '../../helpers/formattedDate';
 import { BasicModal } from '../../components/Modals/BasicModal/BasicModal';
 import DeleteOrder from '../../components/Modals/AdminModals/DeleteOrder';
+import { Filter } from '../../components/Filter/Filter';
 
 const AdminOrders = () => {
   const dispatch = useDispatch();
@@ -30,7 +31,7 @@ const AdminOrders = () => {
     <div>
       <div className="mb-4 grid grid-cols-1 gap-6">
         <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden lg:col-span-2">
-          <div className="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex items-center justify-between p-6">
+        <div className="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex flex-col gap-y-4  md:items-center md:flex-row md:justify-between p-6">
             <div>
               <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-1">
                 Заказы
@@ -58,31 +59,9 @@ const AdminOrders = () => {
                 <p className=" animate-pulse">Идёт загрузка</p>
               )}
             </div>
-            <button
-              aria-expanded="false"
-              aria-haspopup="menu"
-              id=":r5:"
-              className="relative middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30"
-              type="button"
-            >
-              <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currenColor"
-                  viewBox="0 0 24 24"
-                  strokeWidth="3"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-                  ></path>
-                </svg>
-              </span>
-            </button>
+            <div className="">
+              <Filter nameFilter={true}/>
+            </div>
           </div>
           <div className="p-6 overflow-x-scroll px-0 pt-0 pb-[100px]">
             <table className="w-full min-w-[640px] table-auto">
